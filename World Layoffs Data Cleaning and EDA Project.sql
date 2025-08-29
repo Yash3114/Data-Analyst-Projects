6b77d3e4a13ee8884a9af41bf7fdef1ca6ff8a91
Yash <155625356+Yash3114@users.noreply.github.com>
2025-08-29 19:08:16 +0530

    Update and rename World Layoffs Data Cleaning Project.sql to World Layoffs Data Cleaning and EDA Project.sql

--- World Layoffs Data Cleaning and EDA Project.sql
+++ World Layoffs Data Cleaning and EDA Project.sql
@@ -158,47 +158,70 @@ FROM layoffs_staging2;
 
 -- Exploratory Data Analysis
 
+-- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers.
+-- Normally when you start the EDA process you have some idea of what you're looking for.
+-- With this info we are just going to look around and see what we find!
+
 SELECT *
 FROM layoffs_staging2;
 
-SELECT MAX(total_laid_off), MAX(percentage_laid_off)
+-- Looking at Percentage to see how big these layoffs were.
+SELECT MAX(percentage_laid_off), MIN(percentage_laid_off)
 FROM layoffs_staging2;
 
+-- Which companies had 1 which is basically 100 percent of they company laid off.
+-- These are mostly startups it looks like who all went out of business during this time.
 SELECT *
 FROM layoffs_staging2
 WHERE percentage_laid_off = 1
 ORDER BY total_laid_off DESC;
 
-SELECT company, SUM(total_laid_off)
+-- If we order by funcs_raised_millions we can see how big some of these companies were.
+SELECT *
 FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
+WHERE  percentage_laid_off = 1
+ORDER BY funds_raised_millions DESC;
 
-SELECT MAX(`date`), MAX(`date`)
-FROM layoffs_staging2;
+-- SELECT MIN(`date`), MAX(`date`)
+-- FROM layoffs_staging2;
 
+-- Industries with most layoffs from 2020 - 2023
 SELECT industry, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY industry
 ORDER BY 2 DESC;
 
+-- Countries with most layoffs from 2020 - 2023
 SELECT country, SUM(total_laid_off)
 FROM layoffs_staging2
 GROUP BY country
 ORDER BY 2 DESC;
 
-SELECT YEAR(`date`), SUM(total_laid_off)
+-- Companies with most total layoffs from 2020 - 2023
+SELECT company, SUM(total_laid_off)
 FROM layoffs_staging2
-GROUP BY YEAR(`date`)
-ORDER BY 1 DESC;
+GROUP BY company
+ORDER BY 2 DESC;
 
--- Rolling total layoff
-SELECT SUBSTRING(`date`,1,7) AS `MONTH`, SUM(total_laid_off)
+-- Locations with most total layoffs from 2020 - 2023
+SELECT location, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY location
+ORDER BY 2 DESC;
+
+-- Layoffs by stage
+SELECT stage, SUM(total_laid_off)
+FROM world_layoffs.layoffs_staging2
+GROUP BY stage
+ORDER BY 2 DESC;
+
+-- Layoffs by year
+SELECT YEAR('date'), SUM(total_laid_off)
 FROM layoffs_staging2
-WHERE SUBSTRING(`date`,1,7) IS NOT NULL
-GROUP BY `MONTH`
+GROUP BY YEAR('date')
 ORDER BY 1 ASC;
 
+-- Rolling total of layoffs per month
 WITH Rolling_Total AS
 (
 SELECT SUBSTRING(`date`,1,7) AS `Month`, SUM(total_laid_off) AS total_off
@@ -207,40 +230,25 @@ WHERE SUBSTRING(`date`,1,7) IS NOT NULL
 GROUP BY `Month`
 ORDER BY 1 ASC
 )
-
 SELECT `MONTH`, total_off, SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
 FROM Rolling_Total;
 
-SELECT company, SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company
-ORDER BY 2 DESC;
-
-SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company, YEAR(`date`);
-
-WITH 
-Company_Year (company, years, total_laid_off) AS
+-- Top 3 companies with most layoffs in each year.
+WITH Company_Year AS 
 (
-SELECT company, YEAR(`date`), SUM(total_laid_off)
-FROM layoffs_staging2
-GROUP BY company, YEAR(`date`)
-),
-Company_Year_Rank AS
-(
-SELECT *, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
-FROM Company_year
-WHERE years IS NOT NULL
+  SELECT company, YEAR(date) AS years, SUM(total_laid_off) AS total_laid_off
+  FROM layoffs_staging2
+  GROUP BY company, YEAR(date)
 )
-
-SELECT *
+, Company_Year_Rank AS (
+  SELECT company, years, total_laid_off, DENSE_RANK() OVER (PARTITION BY years ORDER BY total_laid_off DESC) AS ranking
+  FROM Company_Year
+)
+SELECT company, years, total_laid_off, ranking
 FROM Company_Year_Rank
-WHERE ranking <= 5;
-
-SELECT *
-FROM layoffs_staging2;
-
+WHERE ranking <= 3
+AND years IS NOT NULL
+ORDER BY years ASC, total_laid_off DESC;
 
 
 
